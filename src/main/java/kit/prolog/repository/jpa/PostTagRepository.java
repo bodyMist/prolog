8bc9270236dc83d8cc01c1872328d99a4f5e147d
bodymist <sky834459@gmail.com>
2022-06-25 12:01:38 +0900

    add repository to save tags

---
+++ src/main/java/kit/prolog/repository/jpa/PostTagRepository.java
@@ -0,0 +1,8 @@
+package kit.prolog.repository.jpa;
+
+import kit.prolog.domain.PostTag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostTagRepository extends JpaRepository<PostTag, Long> {
+
+}
--- src/main/java/kit/prolog/repository/jpa/TagRepository.java
+++ src/main/java/kit/prolog/repository/jpa/TagRepository.java
@@ -4,7 +4,13 @@ import kit.prolog.domain.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
-    public List<Tag> findAllByNameLike(String name);
+    // 검색어가 포함된 태그 이름 모두 조회
+    List<Tag> findAllByNameLike(String name);
+    // 태그 여부 확인
+    boolean existsByName(String name);
+    // 태그 조회(name은 unique key)
+    Optional<Tag> findByName(String name);
 }
