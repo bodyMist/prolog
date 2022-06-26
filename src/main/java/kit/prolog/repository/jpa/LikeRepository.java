c3bc8509be5aa49de45d1e0733cd2f965d11a0bf
bodymist <sky834459@gmail.com>
2022-06-26 21:14:32 +0900

    add a repository and a test to save and delete a like

--- src/main/java/kit/prolog/repository/jpa/LikeRepository.java
+++ src/main/java/kit/prolog/repository/jpa/LikeRepository.java
@@ -3,5 +3,8 @@ package kit.prolog.repository.jpa;
 import kit.prolog.domain.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LikeRepository extends JpaRepository<Like, Long> {
+    Optional<Like> findByUser_IdAndPost_Id(Long userId, Long postId);
 }
--- src/test/java/kit/prolog/repository/PostRepositoryTest.java
+++ src/test/java/kit/prolog/repository/PostRepositoryTest.java
@@ -1,15 +1,22 @@
 package kit.prolog.repository;
 
+import kit.prolog.domain.Like;
+import kit.prolog.domain.Post;
+import kit.prolog.domain.User;
+import kit.prolog.repository.jpa.LikeRepository;
 import kit.prolog.repository.jpa.PostRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @SpringBootTest
 @Transactional
 public class PostRepositoryTest {
     @Autowired private PostRepository postRepository;
+    @Autowired private LikeRepository likeRepository;
 
     @Test
     void 카테고리별_게시글_조회(){
@@ -19,5 +26,19 @@ public class PostRepositoryTest {
         postRepository.findPostByCategoryName(account, categoryName, cursor)
                 .forEach(System.out::println);
 
+    }
+
+    @Test
+    void 게시글_좋아요_취소(){
+        Long userId = 1L, postId = 7L;
+        Optional<Like> like = likeRepository.findByUser_IdAndPost_Id(userId, postId);
+        if(like.isPresent()){
+            likeRepository.delete(like.get());
+            System.out.println("좋아요 취소");
+        }else{
+            // 예외처리 주의
+            likeRepository.save(new Like(new User(userId), new Post(postId)));
+            System.out.println("좋아요 등록");
+        }
     }
 }
