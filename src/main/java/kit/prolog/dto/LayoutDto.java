c6c49e82bc694218e7ece0e5cab3a98da030a39e
bodyMist <sky834459@gmail.com>
2022-06-29 13:18:37 +0900

    add comments for each constructor

--- src/main/java/kit/prolog/dto/LayoutDto.java
+++ src/main/java/kit/prolog/dto/LayoutDto.java
@@ -15,13 +15,24 @@ public class LayoutDto {
     private double height;
     private String context;
 
+    // PostPreviewDto 하위 DTO
     public LayoutDto(Long id, int dtype) {
         this.id = id;
         this.dtype = dtype;
     }
 
+    // 레이아웃 틀 하위 레이아웃 리스트 조회 API
     public LayoutDto(Long id, int dtype, double coordinateX, double coordinateY, double width, double height) {
         this.id = id;
+        this.dtype = dtype;
+        this.coordinateX = coordinateX;
+        this.coordinateY = coordinateY;
+        this.width = width;
+        this.height = height;
+    }
+
+    // 레이아웃 작성 API
+    public LayoutDto(int dtype, double coordinateX, double coordinateY, double width, double height) {
         this.dtype = dtype;
         this.coordinateX = coordinateX;
         this.coordinateY = coordinateY;
