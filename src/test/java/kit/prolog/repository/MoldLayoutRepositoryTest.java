3a46aa2fc062fe76f2f07394e31c1bc8f0c92441
bodyMist <sky834459@gmail.com>
2022-07-06 21:14:16 +0900

    test : add a test to save post, must do projection

--- src/test/java/kit/prolog/repository/MoldLayoutRepositoryTest.java
+++ src/test/java/kit/prolog/repository/MoldLayoutRepositoryTest.java
@@ -1,7 +1,6 @@
 package kit.prolog.repository;
 
-import kit.prolog.domain.Mold;
-import kit.prolog.domain.Post;
+import kit.prolog.domain.*;
 import kit.prolog.dto.LayoutDto;
 import kit.prolog.dto.MoldDto;
 import kit.prolog.repository.jpa.LayoutRepository;
@@ -23,6 +22,41 @@ public class MoldLayoutRepositoryTest {
     @Autowired MoldRepository moldRepository;
     @Autowired LayoutRepository layoutRepository;
     @Autowired PostRepository postRepository;
+
+    @Test
+    void 레이아웃_상속클래스_저장(){
+        Layout layout = layoutRepository.findById(1L).get();
+        String test = "test";
+        Layout testLayout;
+        switch (layout.getDtype()){
+            case 1:
+                testLayout = new Context(test);
+                break;
+            case 2:
+                testLayout = new Image(test);
+                break;
+            case 3:
+                testLayout = new Code(test);
+                break;
+            case 4:
+                testLayout = new Hyperlink(test);
+                break;
+            case 5:
+                testLayout = new Mathematics(test);
+                break;
+            case 6:
+                testLayout = new Video(test);
+                break;
+            case 7:
+                testLayout = new Document(test);
+                break;
+            default:
+                testLayout = new Layout();
+                break;
+        }
+        layoutRepository.save(testLayout);
+        layoutRepository.flush();
+    }
 
     @Test
     void 레이아웃틀_조회(){
--- src/test/java/kit/prolog/service/PostServiceTest.java
+++ src/test/java/kit/prolog/service/PostServiceTest.java
@@ -1,15 +1,34 @@
 package kit.prolog.service;
 
+import kit.prolog.dto.LayoutDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 @SpringBootTest
 @Transactional
 public class PostServiceTest {
     @Autowired private PostService postService;
+
+    @Test
+    void 레이아웃_리스트_저장(){
+        Long userId = 1L;
+        String moldName = "기본틀";
+        List<LayoutDto> layoutList = List.of(new LayoutDto(1, 1.0, 1.0, 1, 1));
+        postService.saveLayouts(userId, moldName, layoutList);
+    }
+
+    @Test
+    void 게시글_작성(){
+        Long userId = 1L, moldId = 1L, categoryId = 1L;
+        String title = "씨발 존나 하기 싫어";
+        List<LayoutDto> layoutList = List.of(new LayoutDto(1L, "콘텐트"), new LayoutDto(2L, "http://"));
+        postService.writePost(userId, moldId, title, layoutList, categoryId);
+    }
 
     @Test
     void 레이아웃틀_cascade(){
