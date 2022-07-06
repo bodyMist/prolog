8c827487ea354f8540c002e5eba9d1db478c5925
bodyMist <sky834459@gmail.com>
2022-07-06 21:07:38 +0900

    feat : add constructor to save inherited Layouts

--- src/main/java/kit/prolog/domain/Image.java
+++ src/main/java/kit/prolog/domain/Image.java
@@ -1,12 +1,19 @@
 package kit.prolog.domain;
 
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity(name = "IMAGES")
 @DiscriminatorValue("2")
+@NoArgsConstructor
 public class Image extends Layout{
     @Column(nullable = false)
     private String url;
+
+    public Image(String url) {
+        this.url = url;
+    }
 }
--- src/main/java/kit/prolog/domain/Video.java
+++ src/main/java/kit/prolog/domain/Video.java
@@ -1,12 +1,19 @@
 package kit.prolog.domain;
 
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity(name = "VIDEOS")
 @DiscriminatorValue("6")
+@NoArgsConstructor
 public class Video extends Layout{
     @Column(nullable = false)
     private String url;
+
+    public Video(String url) {
+        this.url = url;
+    }
 }
