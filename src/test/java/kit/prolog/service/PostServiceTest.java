91fd63664eb62f865c35395d59e6213c941c40ce
bodyMist <sky834459@gmail.com>
2022-07-07 18:36:13 +0900

    test : test savePost API

--- src/test/java/kit/prolog/service/PostServiceTest.java
+++ src/test/java/kit/prolog/service/PostServiceTest.java
@@ -1,38 +1,73 @@
 package kit.prolog.service;
 
+import kit.prolog.domain.*;
+import kit.prolog.dto.AttachmentDto;
 import kit.prolog.dto.LayoutDto;
+import kit.prolog.repository.jpa.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
 
-@SpringBootTest
-@Transactional
+@ExtendWith(MockitoExtension.class)
 public class PostServiceTest {
-    @Autowired private PostService postService;
+    @InjectMocks
+    private PostService postService;
+    @Mock
+    private PostRepository postRepository;
+    @Mock
+    private MoldRepository moldRepository;
+    @Mock
+    private LayoutRepository layoutRepository;
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private CategoryRepository categoryRepository;
+    @Mock
+    private AttachmentRepository attachmentRepository;
+    @Mock
+    private TagRepository tagRepository;
+    @Mock
+    private PostTagRepository postTagRepository;
 
-    @Test
-    void 레이아웃_리스트_저장(){
-        Long userId = 1L;
-        String moldName = "기본틀";
-        List<LayoutDto> layoutList = List.of(new LayoutDto(1, 1.0, 1.0, 1, 1));
-        postService.saveLayouts(userId, moldName, layoutList);
-    }
 
     @Test
     void 게시글_작성(){
+        // given
         Long userId = 1L, moldId = 1L, categoryId = 1L;
-        String title = "씨발 존나 하기 싫어";
+        String title = "게시글 제목";
         List<LayoutDto> layoutList = List.of(new LayoutDto(1L, "콘텐트"), new LayoutDto(2L, "http://"));
-        postService.writePost(userId, moldId, title, layoutList, categoryId);
-    }
+        HashMap<String, Object> param = new HashMap<>();
 
-    @Test
-    void 레이아웃틀_cascade(){
-        Long moldId = 1L;
-        postService.deleteMold(moldId);
+        List<AttachmentDto> attachmentList = List.of(new AttachmentDto(1L, "url"));
+        param.put("attachments", attachmentList);
+        param.put("tags", List.of("react", "spring"));
+
+        Post post = new Post(title, LocalDateTime.now(), new User(userId),
+                Category.builder().id(categoryId).build(), new Mold(moldId));
+
+        when(postRepository.save(any(Post.class))).thenReturn(post);
+        when(moldRepository.findById(moldId)).thenReturn(Optional.of(new Mold(moldId)));
+        when(categoryRepository.findById(categoryId)).thenReturn(Optional.of(Category.builder().id(categoryId).build()));
+        when(userRepository.findById(userId)).thenReturn(Optional.of(new User(userId)));
+
+        Long writePost = postService.writePost(userId, moldId, title, layoutList, categoryId, param);
+
+        assertThat(writePost).isEqualTo(post.getId());
+        verify(tagRepository, times(2)).save(any(Tag.class));
+        verify(attachmentRepository, times(1)).save(any(Attachment.class));
     }
 }
