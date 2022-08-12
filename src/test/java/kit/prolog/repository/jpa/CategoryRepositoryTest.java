6baef34dfd9961f43083e9ab28a3e4db373f609b
gh1224 <rlarkgus0@naver.com>
2022-08-12 17:54:23 +0900

    feat: 카테고리 깊이 관련 요구사항 변경 반영 (카테고리 조회)

--- src/test/java/kit/prolog/repository/jpa/CategoryRepositoryTest.java
+++ src/test/java/kit/prolog/repository/jpa/CategoryRepositoryTest.java
@@ -7,10 +7,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,6 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Import(QuerydslConfig.class)
 @ActiveProfiles("test")
 @Sql(scripts = {"classpath:test.sql"})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class CategoryRepositoryTest {
 
     @Autowired
@@ -32,6 +33,7 @@ class CategoryRepositoryTest {
         Category category = Category.builder()
                 .name("백엔드")
                 .user(userRepository.findById(1L).get())
+                .upperCategory(categoryRepository.findById(1L).get())
                 .build();
         Category savedCategory = categoryRepository.save(category);
 
@@ -41,10 +43,11 @@ class CategoryRepositoryTest {
 
     @Test
     void 카테고리_정보_조회() {
-        List<CategoryInfoDto> expected = new ArrayList();
-        expected.add(new CategoryInfoDto(1L, "개발용", 2L));
-        expected.add(new CategoryInfoDto(2L, "취미용", 3L));
-        expected.add(new CategoryInfoDto(3L, "전시용", 1L));
+        List<CategoryInfoDto> expected = List.of(
+                new CategoryInfoDto(1L, "전체", 2L, List.of(
+                        new CategoryInfoDto(2L, "개발용", 2L,
+                                List.of(new CategoryInfoDto(4L, "전시용", 0L, null))),
+                        new CategoryInfoDto(3L, "취미용", 0L, List.of()))));
 
         assertThat(categoryRepository.findInfoByUserId(1L))
                 .isEqualTo(expected);
