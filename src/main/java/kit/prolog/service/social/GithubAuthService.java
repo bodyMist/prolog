64f82061591d361088891ecd80e76659ccff1629
tkdrms0301 <tkdrms0301@naver.com>
2022-09-05 00:36:08 +0900

    social login properties

--- src/main/java/kit/prolog/service/social/GithubAuthService.java
+++ src/main/java/kit/prolog/service/social/GithubAuthService.java
@@ -3,6 +3,8 @@ package kit.prolog.service.social;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,12 +16,21 @@ import java.net.URL;
 @Service
 @Slf4j
 @RequiredArgsConstructor
+@PropertySource("classpath:social/github.properties")
 public class GithubAuthService {
+    @Value("${github.client_id}")
+    private String client_id; //프론트에서 호출한 uri와 동일하게
+    @Value("${github.client_secret}")
+    private String client_secret;
+    @Value("${github.requestTokenUrl}")
+    private String requestTokenUrl;
+    @Value("${github.requestUserInfoUrl}")
+    private String requestUserInfoUrl;
     public String getGithubAccessToken(String code)  {
         String accessToken = "";
 
         try{
-            URL url = new URL("https://github.com/login/oauth/access_token");
+            URL url = new URL(requestTokenUrl);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setDoInput(true);
             conn.setDoOutput(true);
@@ -27,46 +38,45 @@ public class GithubAuthService {
             conn.setRequestProperty("Accept", "application/json");
             conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36");
 
-            // 이 부분에 client_id, client_secret, code를 넣어주자.
-            // 여기서 사용한 secret 값은 사용 후 바로 삭제하였다.
-            // 실제 서비스나 깃허브에 올릴 때 이 부분은 항상 주의하자.
-
-            // client_secret : 729505c3586bcee7fb0d47b9182eb7060123e5ce
             try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()))) {
-                bw.write("client_id=0006efe23ef0c6ecb6c0&client_secret=729505c3586bcee7fb0d47b9182eb7060123e5ce&code=" + code);
+                StringBuilder sb = new StringBuilder();
+                sb.append("client_id=");
+                sb.append(client_id);
+                sb.append("&client_secret=");
+                sb.append(client_secret);
+                sb.append("&code=");
+                sb.append(code);
+                bw.write(sb.toString());
                 bw.flush();
             }
 
             //결과 코드가 200이라면 성공
             int responseCode = conn.getResponseCode();
-            System.out.println("responseCode : " + responseCode);
+            if(responseCode == 200){
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String line = "";
+                String result = "";
+                while ((line = br.readLine()) != null) {
+                    result += line;
+                }
+                System.out.println("response body : " + result);
 
-            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                //Gson 라이브러리에 포함된 클래스로 JSON파싱 객체 생성
+                JSONObject jObject = new JSONObject(result);
+                accessToken = jObject.get("access_token").toString();
+                System.out.println("access_token : " + accessToken);
 
-            String line = "";
-            String result = "";
-
-            while ((line = br.readLine()) != null) {
-                result += line;
+                br.close();
+                return accessToken;
             }
-            System.out.println("response body : " + result);
-
-            //Gson 라이브러리에 포함된 클래스로 JSON파싱 객체 생성
-            JSONObject jObject = new JSONObject(result);
-            accessToken = jObject.get("access_token").toString();
-
-            System.out.println("access_token : " + accessToken);
-
-            br.close();
         }catch (IOException e){
             e.printStackTrace();
         }
-
-        return accessToken;
+        return null;
     }
 
     public String getGithubUserKey(String access_token) throws IOException {
-        URL url = new URL("https://api.github.com/user");
+        URL url = new URL(requestUserInfoUrl);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         conn.setDoOutput(true);
@@ -75,20 +85,22 @@ public class GithubAuthService {
         conn.setRequestProperty("Authorization", "token " + access_token);
 
         int responseCode = conn.getResponseCode();
-        System.out.println("responseCode : " + responseCode);
+        if(responseCode == 200){
+            //요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line = "";
+            String result = "";
 
-        //요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
-        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        String line = "";
-        String result = "";
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+            System.out.println("response body : " + result);
 
-        while ((line = br.readLine()) != null) {
-            result += line;
+            JSONObject jObject = new JSONObject(result);
+            int socialKey = jObject.getInt("id");
+            return String.valueOf(socialKey);
+        }else{
+            return null;
         }
-        System.out.println("response body : " + result);
-
-        JSONObject jObject = new JSONObject(result);
-        int socialKey = jObject.getInt("id");
-        return String.valueOf(socialKey);
     }
 }
