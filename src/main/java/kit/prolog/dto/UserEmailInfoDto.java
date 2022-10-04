a4813e8e6a86e9999f8437928af715ef45e87356
tkdrms0301 <tkdrms0301@naver.com>
2022-10-04 23:20:03 +0900

    password security

--- src/main/java/kit/prolog/dto/UserEmailInfoDto.java
+++ src/main/java/kit/prolog/dto/UserEmailInfoDto.java
@@ -1,14 +1,11 @@
 package kit.prolog.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @Setter
 @ToString
-@AllArgsConstructor
+@NoArgsConstructor
 public class UserEmailInfoDto {
     private String name;
     private String account;
