a8e0ccf090b02e6af7c9426d88bd1d8b68fac797
bodyMist <sky834459@gmail.com>
2022-10-21 13:02:19 +0900

    update : Attachment' url length from 255 to 2000

--- src/main/java/kit/prolog/domain/Attachment.java
+++ src/main/java/kit/prolog/domain/Attachment.java
@@ -17,7 +17,7 @@ public class Attachment {
     private Long id;
     @Column(nullable = false)
     private String name;
-    @Column(nullable = false)
+    @Column(nullable = false, length = 2000)
     private String url;
     @Column(nullable = false)
     private String extension;
