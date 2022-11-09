021d2cd4730e47a7ca71171f1844b128ca8631db
bodyMist <sky834459@gmail.com>
2022-11-10 02:18:40 +0900

    fix : add param annotation for @query parameter

--- src/main/java/kit/prolog/repository/jpa/PostTagRepository.java
+++ src/main/java/kit/prolog/repository/jpa/PostTagRepository.java
@@ -3,11 +3,12 @@ package kit.prolog.repository.jpa;
 import kit.prolog.domain.PostTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PostTagRepository extends JpaRepository<PostTag, Long> {
     @Query("SELECT t.name FROM POSTS_AND_TAGS pt INNER JOIN TAGS t ON pt.tag.id = t.id WHERE pt.post.id = :postId")
-    List<String> findTagNameByPost_Id(Long postId);
+    List<String> findTagNameByPost_Id(@Param("postId") Long postId);
     void deleteAllByPost_Id(Long postId);
 }
