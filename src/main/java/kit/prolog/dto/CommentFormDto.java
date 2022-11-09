1ac8eca9dbe3b2f703174a4940f22364fd4ed422
gh1224 <rlarkgus0@naver.com>
2022-11-09 22:20:34 +0900

    fix: CommentFormDto 기본 생성자 추가

--- src/main/java/kit/prolog/dto/CommentFormDto.java
+++ src/main/java/kit/prolog/dto/CommentFormDto.java
@@ -2,9 +2,11 @@ package kit.prolog.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CommentFormDto {
     private Long postId;
     private Long upperCommentId;
