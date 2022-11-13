686942a5a692d17a416ccb752464118f6e34a2f2
bodyMist <sky834459@gmail.com>
2022-11-14 02:29:36 +0900

    feat : add LogFilter

---
+++ src/main/java/kit/prolog/filter/LogFilter.java
@@ -0,0 +1,47 @@
+package kit.prolog.filter;
+
+import kit.prolog.util.ClientIpUtil;
+import kit.prolog.util.ReadableRequestWrapper;
+import lombok.extern.log4j.Log4j2;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
+@Log4j2
+public class LogFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        ReadableRequestWrapper readableRequest = new ReadableRequestWrapper((HttpServletRequest) request);
+        String requestIp = ClientIpUtil.getClientIp(readableRequest);
+        log.info("요청 IP: " + requestIp);
+        log.info("요청 URL: " + readableRequest.getRequestURI());
+        String requestMethod = readableRequest.getMethod();
+        if(Objects.equals(requestMethod, "POST")){
+            String json = readBodyData(readableRequest);
+            log.info("요청 정보: {}" + json);
+        }else if(Objects.equals(requestMethod, "GET") || Objects.equals(requestMethod, "DELETE")){
+            log.info("요청 정보: {}" + readableRequest.getQueryString());
+        }else if(Objects.equals(requestMethod, "PUT")){
+            String json = readBodyData(readableRequest);
+            log.info("요청 정보: {}" + json);
+            log.info("요청 정보: {}" + readableRequest.getQueryString());
+        }
+        chain.doFilter(readableRequest,response);
+    }
+    private String readBodyData(HttpServletRequest request) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(request.getInputStream()));
+        StringBuilder builder = new StringBuilder();
+        String buffer;
+        while ((buffer = input.readLine()) != null) {
+            if (builder.length() > 0) {
+                builder.append("\n");
+            }
+            builder.append(buffer);
+        }
+        return builder.toString();
+    }
+}
---
+++ src/main/java/kit/prolog/util/ReadableRequestWrapper.java
@@ -0,0 +1,69 @@
+package kit.prolog.util;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.coyote.http11.upgrade.UpgradeServletInputStream;
+import org.springframework.lang.Nullable;
+
+import javax.servlet.ReadListener;
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.*;
+
+public class ReadableRequestWrapper extends HttpServletRequestWrapper {
+
+    private ByteArrayOutputStream cachedContent;
+    @Nullable
+    private BufferedReader reader;
+
+    public ReadableRequestWrapper(HttpServletRequest request) throws IOException {
+        super(request);
+    }
+
+    @Override
+    public ServletInputStream getInputStream() throws IOException {
+        if (cachedContent == null) cacheInputStream();
+        return new CachedServletInputStream(cachedContent.toByteArray());
+    }
+
+    @Override
+    public BufferedReader getReader() throws IOException {
+        if (this.reader == null) {
+            this.reader = new BufferedReader(new InputStreamReader(getInputStream(), getCharacterEncoding()));
+        }
+        return this.reader;
+    }
+    private void cacheInputStream() throws IOException {
+        cachedContent = new ByteArrayOutputStream();
+        IOUtils.copy(super.getInputStream(), cachedContent);
+    }
+
+    private static class CachedServletInputStream extends ServletInputStream {
+        private final ByteArrayInputStream buffer;
+
+        public CachedServletInputStream(byte[] contents) {
+            this.buffer = new ByteArrayInputStream(contents);
+        }
+
+        @Override
+        public int read() {
+            return buffer.read();
+        }
+
+        @Override
+        public boolean isFinished() {
+            return buffer.available() == 0;
+        }
+
+        @Override
+        public boolean isReady() {
+            return true;
+        }
+
+        @Override
+        public void setReadListener(ReadListener listener) {
+            throw new RuntimeException("Not implemented");
+        }
+    }
+}
